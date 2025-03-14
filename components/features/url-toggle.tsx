a7b6182112c75959e90e4496c5dbbdf440a435b2
Bart Kang <bart.kang.0719@gmail.com>
2025-03-14 15:48:46 -0700

    fix collapsible for long urls

--- components/features/url-toggle.tsx
+++ components/features/url-toggle.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import TooltipContainer from "../core/tooltip-container";
 
 type Props = {
@@ -8,19 +8,30 @@ type Props = {
 };
 
 export default function URLToggle({ url }: Props) {
+  const ref = useRef<HTMLParagraphElement>(null);
   const [isCollapsed, setIsCollapsed] = useState<boolean>(true);
+  const [isCollapsible, setIsCollapsible] = useState<boolean>(true);
 
-  return (
+  useEffect(() => {
+    if (ref.current) {
+      setIsCollapsible(ref.current.scrollHeight > ref.current.clientHeight);
+    }
+  }, []);
+
+  return isCollapsible ? (
     <TooltipContainer message={isCollapsed ? "Expand URL" : "Collapse URL"}>
-      <button
+      <p
         role="button"
-        className={`text-sm text-primary break-all cursor-pointer text-left ${
+        ref={ref}
+        className={`text-sm text-primary break-all text-left cursor-pointer  ${
           isCollapsed && "line-clamp-2"
         }`}
         onClick={() => setIsCollapsed((prev) => !prev)}
       >
-        <span>{url}</span>
-      </button>
+        {url}
+      </p>
     </TooltipContainer>
+  ) : (
+    <p className="text-sm text-primary break-all">{url}</p>
   );
 }
