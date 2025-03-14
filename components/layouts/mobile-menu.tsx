31d9f91621384e021a9d8ff9704949808cf9fd77
Bart Kang <bart.kang.0719@gmail.com>
2025-03-14 13:24:04 -0700

    fix nested buttons

--- components/layouts/mobile-menu.tsx
+++ components/layouts/mobile-menu.tsx
@@ -2,8 +2,8 @@ import Navigation from "@/components/layouts/navigation";
 import { Button } from "@/components/ui/button";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
-  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger
@@ -22,21 +22,18 @@ export default function MobileMenu() {
           <Menu className="size-7" />
         </Button>
       </SheetTrigger>
-      <SheetContent
-        className="w-screen"
-        closeButton={
-          <Button
-            className="bg-transparent hover:bg-black hover:text-primary-foreground size-10"
-            size="icon"
-            variant="outline"
-          >
-            <XIcon className="size-7" />
-          </Button>
-        }
-      >
-        <SheetHeader className="h-[77px] border-b">
+      <SheetContent className="w-screen" closeButton={false}>
+        <SheetHeader className="h-[77px] border-b flex-row justify-between">
           <SheetTitle></SheetTitle>
-          <SheetDescription></SheetDescription>
+          <SheetClose asChild>
+            <Button
+              className="bg-transparent hover:bg-black hover:text-primary-foreground size-10"
+              size="icon"
+              variant="outline"
+            >
+              <XIcon className="size-7" />
+            </Button>
+          </SheetClose>
         </SheetHeader>
 
         <Navigation />
--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -47,12 +47,12 @@ function SheetOverlay({
 function SheetContent({
   className,
   children,
-  closeButton,
+  closeButton = true,
   side = "right",
   ...props
 }: React.ComponentProps<typeof SheetPrimitive.Content> & {
   side?: "top" | "right" | "bottom" | "left";
-  closeButton?: React.ReactNode;
+  closeButton?: boolean;
 }) {
   return (
     <SheetPortal>
@@ -74,16 +74,14 @@ function SheetContent({
         {...props}
       >
         {children}
-        <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
-          {!closeButton ? (
+        {closeButton && (
+          <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
             <>
               <XIcon className="size-4" />
               <span className="sr-only">Close</span>
             </>
-          ) : (
-            closeButton
-          )}
-        </SheetPrimitive.Close>
+          </SheetPrimitive.Close>
+        )}
       </SheetPrimitive.Content>
     </SheetPortal>
   );
